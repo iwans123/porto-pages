754cbbcc36b1dc48910ce7ce0455438b6684c58f
iwans123 <57323144+iwans123@users.noreply.github.com>
2022-03-01 21:34:02 +0700

    deploy

---
+++ precache-manifest.62763b0da0107ce2b2de8b77199040e1.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd2963800bca2e9508f",
+    "url": "/porto-pages/css/about.079f332a.css"
+  },
+  {
+    "revision": "a20570ee4829738c02ce",
+    "url": "/porto-pages/css/app.4e879386.css"
+  },
+  {
+    "revision": "fb227d3858516ff06a05",
+    "url": "/porto-pages/css/profile.eec58658.css"
+  },
+  {
+    "revision": "480871198bc4c89c1729783b75a4c7e6",
+    "url": "/porto-pages/fonts/Buggy.48087119.otf"
+  },
+  {
+    "revision": "c2e6acd097fb7e8b29da044c85a90320",
+    "url": "/porto-pages/fonts/Scott Room.c2e6acd0.ttf"
+  },
+  {
+    "revision": "41071dfd91b41eecff4ac9da17a59102",
+    "url": "/porto-pages/fonts/You and Me.41071dfd.ttf"
+  },
+  {
+    "revision": "b33205f3ae5708fa3a354e0ac722e575",
+    "url": "/porto-pages/img/akun.b33205f3.png"
+  },
+  {
+    "revision": "ad1fec6bc8619397d27cfdb5e96a716a",
+    "url": "/porto-pages/img/card.ad1fec6b.jpg"
+  },
+  {
+    "revision": "a04c055e3af0c2e3257f3ef8b915bc81",
+    "url": "/porto-pages/img/contact.a04c055e.svg"
+  },
+  {
+    "revision": "a2caa3fe2865b37049ca538c2fa04820",
+    "url": "/porto-pages/img/jumbotron.a2caa3fe.png"
+  },
+  {
+    "revision": "a2caa3fe2865b37049ca538c2fa04820",
+    "url": "/porto-pages/img/picture/jumbotron.png"
+  },
+  {
+    "revision": "68e3a0b7b1d32bd081bc3dce0ab2268c",
+    "url": "/porto-pages/index.html"
+  },
+  {
+    "revision": "6dd2963800bca2e9508f",
+    "url": "/porto-pages/js/about.79181525.js"
+  },
+  {
+    "revision": "a20570ee4829738c02ce",
+    "url": "/porto-pages/js/app.4afdaab2.js"
+  },
+  {
+    "revision": "2abb004080de2f8ede19",
+    "url": "/porto-pages/js/chunk-vendors.a1c4f43d.js"
+  },
+  {
+    "revision": "fb227d3858516ff06a05",
+    "url": "/porto-pages/js/profile.ae85e753.js"
+  },
+  {
+    "revision": "e97c5160991cd3ef67cb4ebb1761dde9",
+    "url": "/porto-pages/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/porto-pages/robots.txt"
+  }
+]);
